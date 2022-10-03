bd61d99742df975a5b10a7239414efbd4e2c558c
milesdwilliams15 <milesdwilliams15@gmail.com>
2022-10-03 11:55:34 -0400

    fix bug

--- R/set_palette.R
+++ R/set_palette.R
@@ -22,32 +22,24 @@ set_palette <- function(
   if(is.null(qualitative)) {
     qual <<- coolors("https://coolors.co/265a73-e8c547-b0bbbf-a85751-331832")
   } else {
-    qual <<- ifelse(from_coolors,
-                    coolors(qualitative),
-                    qualitative)
+    if(from_coolors) qual <<- coolors(qualitative) else qual <<- qualitative
   }
   if(is.null(diverging)) {
     dive <<- coolors("https://coolors.co/265a73-eff1f3-df2935")
   } else {
-    dive <<- ifelse(from_coolors,
-                    coolors(diverging),
-                    diverging)
+    if(from_coolors) dive <<- coolors(diverging) else dive <<- diverging
     if(length(dive)!=3) stop("Diverging palette must have 3 colors! You have: ", length(dive))
   }
   if(is.null(sequential)) {
     sequ <<- coolors("https://coolors.co/eff1f3-265a73")
   } else {
-    sequ <<- ifelse(from_coolors,
-                    coolors(sequential),
-                    sequential)
+    if(from_coolors) sequ <<- coolors(sequential) else qual <<- sequential
     if(length(sequ)!=2) stop("Sequential palette must have 2 colors! You have: ", length(sequ))
   }
   if(is.null(binary)) {
     dual <<- coolors("https://coolors.co/faa916-265a73")
   } else {
-    dual <<- ifelse(from_coolors,
-                    coolors(binary),
-                    binary)
+    if(from_coolors) dual <<- coolors(binary) else qual <<- binary
     if(length(dual)!=2) stop("Binary palette must have 2 colors! You have: ", length(dual))
   }
 }
